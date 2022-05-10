76ccb0cba33a4db973bde28395f3758a0aca7e0d
santiago clemenzi <clemenzisantiago@gmail.com>
2022-05-10 19:19:19 -0300

    creamos ruta

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,11 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+// use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    // #[Route('/user', name: 'app_user')]
     public function index(): Response
     {
         return $this->render('user/index.html.twig', [
